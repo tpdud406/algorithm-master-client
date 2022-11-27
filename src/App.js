6bfb6bd3014a3278d6335539cf61a0e7c0911358
tpdud406 <tpdud406@naver.com>
2022-11-28 02:31:24 +0900

    feat: firebase설정, 로그인, 로그이웃, router설정 (#2)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,63 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import styled from "styled-components";
+import { login, logout, onUserStateChange } from "../../services/firebase";
+import User from "../User";
 
 function Header() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+
+  const handleLogin = async () => {
+    try {
+      const { user } = await login();
+      const idToken = await user.getIdToken();
+
+      await axios.post(
+        `${process.env.REACT_APP_SERVER_HOST}/auth/login`,
+        {
+          name: user.displayName,
+          email: user.email,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${idToken}`,
+          },
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
   return (
     <Wrapper>
       <h1>Algorithm Master</h1>
-      <Button type="button">로그인</Button>
+      {user && <User user={user} />}
+      {!user && (
+        <Button type="button" onClick={handleLogin}>
+          로그인
+        </Button>
+      )}
+      {user && (
+        <Button type="button" onClick={handleLogout}>
+          로그아웃
+        </Button>
+      )}
     </Wrapper>
   );
 }
