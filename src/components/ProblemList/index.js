967d4cec86e3932e95c67695bf19f619d03f8c25
tpdud406 <tpdud406@naver.com>
2022-12-07 17:13:19 +0900

    chore: 빌드파일 수정중

--- src/components/ProblemList/index.js
+++ src/components/ProblemList/index.js
@@ -15,12 +15,12 @@ function ProblemList() {
           `${process.env.REACT_APP_SERVER_HOST}/users/${user_id}/problems`
         );
         console.log("ProblemList response", response);
-        setProblems(response.data);
+        setProblems(() => [...response.data]);
       } catch (err) {
         console.log(err);
       }
     },
-    [problems]
+    []
   );
 
   console.log("ProblemList problems", problems);
